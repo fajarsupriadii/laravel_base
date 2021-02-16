daae461541a87639a95c4ac123eae546941656f2
DESKTOP-I133UR5\Home <fajarsupriadi@gmail.com>
2021-02-16 18:30:37 +0700

    jwt completed

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+
+Route::group([
+    'middleware' => 'jwt.verify',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@getAuthenticatedUser');
 });
